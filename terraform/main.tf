3df72174e3e4e8dea791e964237e3cd727f32115
cnjiru <cnjiru@users.noreply.github.com>
2024-04-08 09:49:59 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "myec2" {
     source = "./modules/ec2"
     instance_type = var.instance_type
     role            = "app"
-    application     = "shop"
+    application     = "ecore"
     company_prefix = var.nm_prefix
     environment    = var.ec2_environment
 }
